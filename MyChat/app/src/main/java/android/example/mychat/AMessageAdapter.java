7a533e29883f354c4b188040ebe30485cf2918b5
demosfen10 <ilya.eliseev.1999@mail.ru>
2020-06-06 00:05:59 +0300

    1.0.0

---
+++ MyChat/app/src/main/java/android/example/mychat/AMessageAdapter.java
@@ -0,0 +1,113 @@
+package android.example.mychat;
+
+import android.app.Activity;
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class AMessageAdapter extends ArrayAdapter <AMessage>{
+
+    private List<AMessage> messages;
+    private Activity activity;
+
+    public AMessageAdapter(Activity context, int resource, List<AMessage> messages)
+    {
+        super(context,resource,messages);
+        this.messages=messages;
+        this.activity=context;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+        LayoutInflater layoutInflater =
+                (LayoutInflater)activity.getSystemService(
+                        Activity.LAYOUT_INFLATER_SERVICE);
+
+        AMessage aMessage = getItem(position);
+        int layoutResource = 0;
+        int viewType = getItemViewType(position);
+
+        if (viewType == 0) {
+            layoutResource = R.layout.my_message_item;
+        } else {
+            layoutResource = R.layout.your_message_item;
+        }
+
+        if (convertView != null) {
+            viewHolder = (ViewHolder) convertView.getTag();
+        } else {
+            convertView = layoutInflater.inflate(
+                    layoutResource, parent, false
+            );
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+
+        boolean isText = aMessage.getImageUrl() == null;
+
+        if (isText) {
+            viewHolder.messageTextView.setVisibility(View.VISIBLE);
+            viewHolder.photoImageView.setVisibility(View.GONE);
+            viewHolder.messageTextView.setText(aMessage.getText());
+            viewHolder.nameTextView.setText(aMessage.getName());
+        } else {
+            viewHolder.nameTextView.setText(aMessage.getName());
+            viewHolder.messageTextView.setVisibility(View.GONE);
+            viewHolder.photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(viewHolder.photoImageView.getContext())
+                    .load(aMessage.getImageUrl())
+                    .into(viewHolder.photoImageView);
+        }
+
+        return convertView;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        int flag;
+        AMessage aMessage = messages.get(position);
+        if (aMessage.isMine()) {
+            flag = 0;
+        } else {
+            flag = 1;
+        }
+
+        return flag;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    private class ViewHolder {
+
+        private ImageView photoImageView;
+        private TextView messageTextView;
+        private TextView nameTextView;
+
+        public ViewHolder(View view) {
+            photoImageView = view.findViewById(R.id.photoImageView);
+            messageTextView = view.findViewById(R.id.messageTextView);
+            nameTextView = view.findViewById(R.id.nameTextView);
+        }
+
+    }
+}
